603154bac8e723a7364476b6d8d544246093a619
pattonwebz <will@pattonwebz.com>
2024-01-19 13:33:26 +0000

    Add a footer with my name and licensing

--- src/index.tsx
+++ src/index.tsx
@@ -31,28 +31,37 @@ const App = () => {
   }, []);
 
   return (
-    <main className="content container">
-      <div className="columns">
-        <div className="column is-5">
-          <h1 className="title">QR Code Generator</h1>
-          <p className="subtitle">
-            This is the simplest QR code generator I could come up with to fill my needs.
-          </p>
-          <InputFields
-            qrCodeValue={qrCodeValue}
-            setQrCodeValue={setQrCodeValue}
-            qrCodes={qrCodes}
-            setQrCodes={setQrCodes}
-            />
-        </div>
-        <div className="column">
-          <QrCodePanel qrCodeValue={qrCodeValue}/>
-        </div>
-        <div className="column is-variable">
-          <SavedQrCodes qrCodes={qrCodes} setQrCodes={setQrCodes} setQrCodeValue={setQrCodeValue}/>
-        </div>
-      </div>
-    </main>
+    <>
+      <main className="content container hero is-fullheight">
+          <div className="columns">
+              <div className="column is-5">
+                  <h1 className="title">QR Code Generator</h1>
+                  <p className="subtitle">
+                      This is the simplest QR code generator I could come up with to fill my needs.
+                  </p>
+                  <InputFields
+                      qrCodeValue={qrCodeValue}
+                      setQrCodeValue={setQrCodeValue}
+                      qrCodes={qrCodes}
+                      setQrCodes={setQrCodes}
+                  />
+              </div>
+              <div className="column">
+                  <QrCodePanel qrCodeValue={qrCodeValue}/>
+              </div>
+              <div className="column is-variable">
+                  <SavedQrCodes qrCodes={qrCodes} setQrCodes={setQrCodes} setQrCodeValue={setQrCodeValue}/>
+              </div>
+          </div>
+      </main>
+      <footer className="footer navbar is-fixed-bottom">
+          <div className="navbar-item is-fullwidth has-text-centered">
+              <p>
+                  <strong>Simple QR Code Generator</strong> by <a href="https://www/pattonwebz.com/">William Patton</a>. The source code is licensed <a href="https://opensource.org/licenses/isc-license.php">ISC</a>. The website content is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+              </p>
+          </div>
+      </footer>
+    </>
   )
 };
 
